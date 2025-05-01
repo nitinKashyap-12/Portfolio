f51ebbbdb803fd8b6f81b744c32b5c4b4916d0e5
nitinKashyap-12 <121217636+nitinKashyap-12@users.noreply.github.com>
2025-05-01 09:07:35 +0530

    Add files via upload

---
+++ portfolio/app.js
@@ -0,0 +1,13 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+
+document.querySelectorAll('.card').forEach(card =>{
+    card.addEventListener('mousemove', e =>{
+        const rect = card.getBoundingClientRect();
+        card.style.setProperty('--x', `${e.clientX - rect.left}px`);
+        card.style.setProperty('--y', `${e.clientY - rect.top}px`);
+    })
+})
